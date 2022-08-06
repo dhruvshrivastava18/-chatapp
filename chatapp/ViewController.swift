004bb5ac97200bec10b4003b339b37fde721dd53
DHRUV SHRIVASTAVA <dhruvshrivastava18@gmail.com>
2022-08-06 17:12:53 +0530

    Initial Commit

---
+++ chatapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatapp
+//
+//  Created by Dhruv Shrivastava on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
